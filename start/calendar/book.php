5f8988276cc80743e39d8c0b1a8779283e02b966
Irina Hrapane <irina.hrapane@gmail.com>
2022-03-23 10:07:18 +0200

    5 dala

--- start/calendar/book.php
+++ start/calendar/book.php
@@ -7,15 +7,21 @@ if(isset($_GET['date'])){
     $bookings = array();
     if($stmt->execute()){
         $result = $stmt->get_result();
+        if($result->num_rows>0){
+            while($row = $result->fetch_assoc()){
+                $bookings[] = $row['timeslot'];
+            }
+            $stmt->close();
+        }
     }
 }
-
+//part5
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $timeslot = $_POST['timeslot'];
-    $stmt = $mysqli->prepare("select * from bookings where date = ?");
-    $stmt->bind_param('s', $date);
+    $stmt = $mysqli->prepare("select * from bookings where date = ? AND timeslot=?");
+    $stmt->bind_param('ss', $date, $timeslot);
     if($stmt->execute()){
         $result = $stmt->get_result();
         if($result->num_rows>0){
@@ -25,12 +31,13 @@ if(isset($_POST['submit'])){
             $stmt->bind_param('ssss', $name, $timeslot, $email, $date);
             $stmt->execute();
             $msg = "<div class='alert alert-success'>Booking Successfull</div>";
+            $bookings[] = $timeslot;
             $stmt->close();
             $mysqli->close();
         }
     }
 }
-//3 dala mainīgie
+
 $duration = 30;
 $cleanup = 0;
 $start = "09:00";
@@ -61,7 +68,6 @@ function timeslots($duration, $cleanup, $start, $end){
 
 
 ?>
-
 <!doctype html>
 <html lang="en">
 
@@ -81,25 +87,29 @@ function timeslots($duration, $cleanup, $start, $end){
         <h1 class="text-center">Book for Date: <?php echo date('m/d/Y', strtotime($date)); ?></h1><hr>
 		
 		
-		<div class="row">
-   <div class="col-md-12">
-       <?php echo(isset($msg))?$msg:""; ?>
-   </div>
-    <?php $timeslots = timeslots($duration, $cleanup, $start, $end); 
-        foreach($timeslots as $ts){
-    ?>
-    <div class="col-md-2">
-        <div class="form-group">
-           <button class="btn btn-success book" data-timeslot="<?php echo $ts; ?>"><?php echo $ts; ?></button>
-          
-
-        </div>
+<div class="row">
+<div class="col-md-12">
+   <?php echo(isset($msg))?$msg:""; ?>
+</div>
+<?php $timeslots = timeslots($duration, $cleanup, $start, $end); 
+    foreach($timeslots as $ts){
+?>
+<div class="col-md-2">
+    <div class="form-group">
+       <?php if(in_array($ts, $bookings)){ ?>
+       <button class="btn btn-danger"><?php echo $ts; ?></button>
+       <?php }else{ ?>
+       <button class="btn btn-success book" data-timeslot="<?php echo $ts; ?>"><?php echo $ts; ?></button>
+       <?php }  ?>
     </div>
-    <?php } ?>
-</div>    
+</div>
+<?php } ?>
+</div>
+
+
     </div>
 	
-	//part4
+	
 	<div id="myModal" class="modal fade" role="dialog">
         <div class="modal-dialog">
 
@@ -137,7 +147,7 @@ function timeslots($duration, $cleanup, $start, $end){
 
         </div>
     </div>
-	//part4
+	
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <?php
@@ -152,4 +162,5 @@ $(".book").click(function(){
 });
 </script>
   </body>
+
 </html>
